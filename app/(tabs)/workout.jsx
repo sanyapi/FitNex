f929718a97b46337082f169ea178c96e53a5300b
Charmagne Jane Duyag <cduyag@up.edu.ph>
2024-05-25 21:54:50 +0800

    Revert "Merge branch 'angel' into charms"
    This reverts commit 1cd74716d4d2872565af26d167676c213baa062c, reversing
    changes made to 0e92736dc5ff72c384014b08e0a53cc61c67f95d.

--- app/(tabs)/workout.jsx
+++ app/(tabs)/workout.jsx
@@ -4,35 +4,6 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 const Workout = () => {
-  const [exercises, setExercises] = useState([]);
-  const route = useRoute();
-  const { bodyPart } = route.params;
-
-  useEffect(() => {
-    const fetchExercises = async () => {
-      try {
-        const response = await axios.get(`https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`, {
-          headers: {
-            'X-RapidAPI-Key': 'ae9a37384bmsh944c7afa7311accp1841ebjsn63a6306e0b29', // Replace with your actual RapidAPI key
-            'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-          },
-        });
-        setExercises(response.data);
-      } catch (error) {
-        console.error('Error fetching exercises:', error);
-      }
-    };
-
-    fetchExercises();
-  }, [bodyPart]);
-
-  const renderExerciseItem = ({ item }) => (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', padding: 20 }}>
-      <Text>{item.name}</Text>
-      <Text>{item.target}</Text>
-    </View>
-  );
-
   return (
     <GestureHandlerRootView>
       <SafeAreaView className="bg-primary h-full">
